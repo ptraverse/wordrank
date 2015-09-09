473f9b5017aa9c034595564f0769122e75cb8e08
Philippe Traverse <philippe.traverse@gmail.com>
2015-09-09 00:46:32 -0700

    Saving to MongoDB wuhu

--- wordrank.js
+++ wordrank.js
@@ -1,20 +1,33 @@
 var exec = require('child_process').exec;
+var db = require('mongoskin').db('mongodb://localhost:27017/wordrank');
 
 /** Scrape Word Directly From Web using PhantomJS Wrapper */
 var _scrapeWord = function(word, callback) {
 	var execString = './node_modules/phantomjs/bin/phantomjs ./wordScrape.js '+word;
 	var result = exec(execString, function (err, stdout, stderr) {
 		if (err) {
-			console.log(err);
-			exit(0);
+			throw err;
 		}
-		callback(stdout.trim());
+		var rank = stdout.trim();
+		// Save the result to MongoDb
+		_dbCreateWord(word, rank);
+		callback(rank);
 	});
 };
 
 /** Save Results to MongoDB **/
-var _dbCreateWord = function(word, callback) {
-	throw "not yet implemented";
+var _dbCreateWord = function(word, rank, callback) {
+	var json = { 'word': word, 'rank': rank };
+	console.log("about to insert!");
+	console.log(json);
+	console.log(db);
+	db.bind('words');
+	db.words.insert(json, function(err) {
+		if (err) {
+			throw err;
+		}
+		console.log("inserted!");
+	});
 };
 var _dbReadWord = function(word, callback) {
 	throw "not yet implemented";
