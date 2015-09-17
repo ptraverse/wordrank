f94930c1b204550f4d86f7d2bfac27e3a2e8b5e2
Philippe Traverse <philippe.traverse@gmail.com>
2015-09-17 01:14:11 -0700

    good stuff

--- public/js/demo.js
+++ public/js/demo.js
@@ -2,14 +2,24 @@
 
 $(document).ready(function() {
 
+	var buildWordUsage = function(word, rank) {
+		var usage = "var wc = require('wordrank-client');\nvar word = '"+word+"';\nvar result = wc.getWord(word);\nvar rank = result.rank; // => "+rank+"\n";
+		return usage;
+	};
+
 	$('input#inputWord').change(function() {
 		var inputWord = $(this).val();
 		console.log("Getting Rank via Ajax for "+inputWord);
 		$('div#result').html('... ');
+		$('div#getUrl').html('... ');
+		$('div#usage').html('... ');
 		$.get('/word/'+inputWord, function(jsonResult) {
 			console.log(jsonResult);
 			console.log(jsonResult.response.rank);
 			$('div#result').html(jsonResult.response.rank);
+			var getHref = '/word/'+inputWord;
+			$('div#getUrl').html('<a href="'+getHref+'"">'+'GET'+'</a><br/>');
+			$('div#usage').html('<pre>'+buildWordUsage(inputWord,jsonResult.response.rank)+'</pre><br/>');
 		}).fail( function() {
 			console.log('fail');
 			$('div#result').html('fail');
